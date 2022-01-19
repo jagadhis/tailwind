2e44d6690843f24992d26bcbf6ed38fe0a77f2ad
jagadhis_0220 <jwaran78@gmail.com>
2022-01-19 13:55:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-[100vh] bg-[#343da3] flex flex-col items-center justify-center sm">
+    <div className="bg-[#5561E5] p-5   items-start justify-center flex flex-col w-10/12 rounded-lg">
+      <h1 className="text-[3em] text-[#fff] font-bold">
+        Hello 👋, This is jagadhiswaran!
+      </h1>
+      <p className="text-[#fff]">
+      I am a React Developer With Around 1.5 years Experince And looking to Work Forward In companies where I can Make A great change and upgrade me as possible
+      </p>
+      <button className="p-2 mt-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-md">Hire Me</button>
     </div>
+   
+    <div className="bg-[#5561E5] p-5 flex  items-start justify-center flex-col w-10/12 rounded-lg ">
+    <h1 className="text-[3em] text-[#fff] font-bold">
+        Hello 👋, This is jagadhiswaran!
+      </h1>
+      <p className="text-[#fff]">
+      I am a React Developer With Around 1.5 years Experince And looking to Work Forward In companies where I can Make A great change and upgrade me as possible
+      </p>
+      <button className="p-2 mt-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-md">Hire Me</button>
+      
+    </div>
+  </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
-  content: [],
+  mode:'jit',
+  content: [
+    
+    './src/**/*.js',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        msm: { max: "640px" },
+        mmd: { max: "768px" },
+        mlg: { max: "1024px" },
+        mxl: { max: "1280px" },
+        m2xl: { max: "1536px" },
+    },
   },
   plugins: [],
+}
 }
