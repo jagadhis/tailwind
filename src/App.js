543189ac94013b1e7326ae3827f99c39c7db62ee
jagadhis_0220 <jwaran78@gmail.com>
2022-01-19 15:23:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@
 
 function App() {
   return (
-    <div className="h-[100vh] bg-[#343da3] flex flex-col items-center justify-center sm">
-    <div className="bg-[#5561E5] p-5   items-start justify-center flex flex-col w-10/12 rounded-lg">
+    <div className="h-100 bg-[#343da3] flex flex-col items-center space-y-4 justify-center sm">
+    <div className="bg-[#5561E5] p-5   items-start justify-center flex flex-col w-10/12  rounded-lg">
       <h1 className="text-[3em] text-[#fff] font-bold">
         Hello 👋, This is jagadhiswaran!
       </h1>
@@ -15,6 +15,36 @@ function App() {
       <button className="p-2 mt-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-md">Hire Me</button>
     </div>
    
+    <div className="bg-[#5561E5] p-5 flex  items-start justify-center flex-col w-10/12 rounded-lg ">
+    <h1 className="text-[3em] text-[#fff] font-bold">
+        Hello 👋, This is jagadhiswaran!
+      </h1>
+      <p className="text-[#fff]">
+      I am a React Developer With Around 1.5 years Experince And looking to Work Forward In companies where I can Make A great change and upgrade me as possible
+      </p>
+      <button className="p-2 mt-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-md">Hire Me</button>
+      
+    </div>
+    <div className="bg-[#5561E5] p-5 flex  items-start justify-center flex-col w-10/12 rounded-lg ">
+    <h1 className="text-[3em] text-[#fff] font-bold">
+        Hello 👋, This is jagadhiswaran!
+      </h1>
+      <p className="text-[#fff]">
+      I am a React Developer With Around 1.5 years Experince And looking to Work Forward In companies where I can Make A great change and upgrade me as possible
+      </p>
+      <button className="p-2 mt-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-md">Hire Me</button>
+      
+    </div>
+    <div className="bg-[#5561E5] p-5 flex  items-start justify-center flex-col w-10/12 rounded-lg ">
+    <h1 className="text-[3em] text-[#fff] font-bold">
+        Hello 👋, This is jagadhiswaran!
+      </h1>
+      <p className="text-[#fff]">
+      I am a React Developer With Around 1.5 years Experince And looking to Work Forward In companies where I can Make A great change and upgrade me as possible
+      </p>
+      <button className="p-2 mt-4 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded-md">Hire Me</button>
+      
+    </div>
     <div className="bg-[#5561E5] p-5 flex  items-start justify-center flex-col w-10/12 rounded-lg ">
     <h1 className="text-[3em] text-[#fff] font-bold">
         Hello 👋, This is jagadhiswaran!
